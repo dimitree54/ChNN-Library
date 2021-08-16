358e39675aeb2216eac73fb87296f556a489a6aa
dimitree54 <dimitree54@gmail.com>
2021-08-16 21:43:43 +0300

    docs for NeuralNetworkWithInput.kt

--- src/main/we/rashchenko/networks/NeuralNetworkWithInput.kt
+++ src/main/we/rashchenko/networks/NeuralNetworkWithInput.kt
@@ -1,8 +1,18 @@
 package we.rashchenko.networks
 
 import we.rashchenko.neurons.inputs.InputNeuron
+import we.rashchenko.neurons.Neuron
 
+/**
+ * [NeuralNetwork] where some [Neuron]-nodes specially marked as [InputNeuron]. To understand what privileges [InputNeuron] has check its documentation.
+ */
 interface NeuralNetworkWithInput : NeuralNetwork {
 	val inputNeuronIDs: Collection<Int>
+
+	/**
+	 * Add [InputNeuron] node to the graph.
+	 * @return newly generated id for the added [InputNeuron].
+	 * Now that [InputNeuron] will be referred with that id in [neuronIDs] and [connections]
+	 */
 	fun addInputNeuron(neuron: InputNeuron): Int
 }
