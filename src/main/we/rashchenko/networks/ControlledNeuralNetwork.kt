38f009936f51f4fe8cf0273f0658f0e5bdfde0a5
dimitree54 <dimitree54@gmail.com>
2021-08-17 01:09:25 +0300

    docs for ControlledNeuralNetwork.kt

--- src/main/we/rashchenko/networks/ControlledNeuralNetwork.kt
+++ src/main/we/rashchenko/networks/ControlledNeuralNetwork.kt
@@ -3,10 +3,28 @@ package we.rashchenko.networks
 import we.rashchenko.base.Feedback
 import we.rashchenko.networks.controllers.NeuralNetworkController
 import we.rashchenko.neurons.ControlledNeuron
-import we.rashchenko.neurons.inputs.InputNeuron
 import we.rashchenko.neurons.Neuron
+import we.rashchenko.neurons.inputs.InputNeuron
 import java.util.*
 
+/**
+ * That [NeuralNetwork] is a wrapper for any other [baseNeuralNetwork] that wraps all newly added [Neuron]
+ *  with [ControlledNeuron] in order to sometimes (with [auditProbability]) audit [Neuron] behaviour.
+ * We do it only sometimes because such audit increase [Neuron] touch time which for big amount of neurons may slow down
+ * [tick] for [NeuralNetwork].
+ * So, choose [auditProbability] based on what [controller] you use.
+ * Based on that audit results for all the neurons [controller] can use that global information to estimate neurons'
+ *  relative quality (lets name that type of feedback as external).
+ * [ControlledNeuralNetwork] not only wraps all the neurons with [ControlledNeuron], but also changes the [getFeedback]
+ *  behaviour.
+ * [ControlledNeuralNetwork] replaces [getFeedback] of the [baseNeuralNetwork] (lets call it internal feedback)
+ *  with the weighted sum of internal and external feedback.
+ * The weight of the sum determined by [controllerFeedbackWeight].
+ * Note that external [Feedback] have to be calculated for all neurons in the [NeuralNetwork] (even for not active),
+ *  so it is even more expensive than [ControlledNeuron] audit (which is called only for active neurons).
+ * To not let it slow down each [tick] that external feedback for all neurons collected not on each tick,
+ *  but periodically (with [updateControllerFeedbackPeriod] period).
+ */
 class ControlledNeuralNetwork(
 	private val baseNeuralNetwork: NeuralNetworkWithInput,
 	private val controller: NeuralNetworkController,
@@ -64,8 +82,8 @@ class ControlledNeuralNetwork(
 	}
 
 	override fun getFeedback(neuronID: Int): Feedback? {
-		return getControllerFeedback(neuronID)?.let { controllerFeedback ->
-			getCollaborativeFeedback(neuronID)?.let { collaborativeFeedback ->
+		return getExternalFeedback(neuronID)?.let { controllerFeedback ->
+			getInternalFeedback(neuronID)?.let { collaborativeFeedback ->
 				Feedback(
 					collaborativeFeedback.value * (1 - controllerFeedbackWeight) +
 							controllerFeedback.value * controllerFeedbackWeight
@@ -74,6 +92,13 @@ class ControlledNeuralNetwork(
 		}
 	}
 
-	fun getControllerFeedback(neuronID: Int): Feedback? = controllerFeedbacks[neuronID]
-	fun getCollaborativeFeedback(neuronID: Int): Feedback? = baseNeuralNetwork.getFeedback(neuronID)
+	/**
+	 * Since [getFeedback] now returns weighted sum of internal and external feedbacks, we need [getExternalFeedback] and [getInternalFeedback] to get original values of external and internal feedback. We need that mainly for logging and visualisation purposes, that functions are not used by [Evolution].
+	 */
+	fun getExternalFeedback(neuronID: Int): Feedback? = controllerFeedbacks[neuronID]
+
+	/**
+	 * Since [getFeedback] now returns weighted sum of internal and external feedbacks, we need [getExternalFeedback] and [getInternalFeedback] to get original values of external and internal feedback. We need that mainly for logging and visualisation purposes, that functions are not used by [Evolution].
+	 */
+	fun getInternalFeedback(neuronID: Int): Feedback? = baseNeuralNetwork.getFeedback(neuronID)
 }
