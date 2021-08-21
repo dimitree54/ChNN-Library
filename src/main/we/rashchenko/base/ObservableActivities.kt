1e4d2d4958fc47ad77aaf660452b03cb87498303
dimitree54 <dimitree54@gmail.com>
2021-08-21 23:02:06 +0700

    docs for ObservableActivities.kt

--- src/main/we/rashchenko/base/ObservableActivities.kt
+++ src/main/we/rashchenko/base/ObservableActivities.kt
@@ -1,5 +1,12 @@
 package we.rashchenko.base
 
+import we.rashchenko.networks.NeuralNetwork
+import we.rashchenko.environments.Environment
+
+/**
+ * If some Collection<[Activity]> (for example [NeuralNetwork] or [Environment] want to make some if these activities
+ *  public it may implement these interface to allow other instances to observe these activities.
+ */
 interface ObservableActivities {
 	val activities: Collection<Activity>
 }
