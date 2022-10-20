4ff3a2550cc5205bc6960b9c3de858d8595f5326
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-10-20 11:46:52 +0300

    separate method for new nodes for ConnectionsAdvisor interface

--- src/main/we/rashchenko/chnn/network/ConnectionsAdvisor.kt
+++ src/main/we/rashchenko/chnn/network/ConnectionsAdvisor.kt
@@ -5,6 +5,7 @@ import org.jgrapht.graph.DefaultEdge
 import we.rashchenko.chnn.node.Node
 
 interface ConnectionsAdvisor<ActivationType, FeedbackType> {
+    fun requestConnectionsForNewNode(): List<Node<ActivationType, FeedbackType>>
     fun requestExtraConnection(node: Node<ActivationType, FeedbackType>): List<Node<ActivationType, FeedbackType>>
     fun requestDropConnection(requestingNode: Node<ActivationType, FeedbackType>, connectedNode: Node<ActivationType, FeedbackType>): Boolean
 }
