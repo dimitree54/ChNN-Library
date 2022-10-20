1811e89556860d602f23b5efaae246e45491371f
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-10-20 11:43:56 +0300

    externally controlled node now can work as base node when getExternalFeedback returns null

--- src/main/we/rashchenko/chnn/environment/ExternallyControlledNode.kt
+++ src/main/we/rashchenko/chnn/environment/ExternallyControlledNode.kt
@@ -4,15 +4,15 @@ import we.rashchenko.chnn.node.Node
 
 abstract class ExternallyControlledNode<ActivationType, FeedbackType>(protected val baseNode: Node<ActivationType, FeedbackType>) :
     Node<ActivationType, FeedbackType> by baseNode {
-    var externalActivity: ActivationType? = null
+    abstract var externalActivity: ActivationType?
 
     override fun getActivation(): ActivationType {
         return externalActivity ?: baseNode.getActivation()
     }
 
     override fun reportFeedbacks(feedbacks: List<FeedbackType>) {
-        baseNode.reportFeedbacks(listOf(getExternalFeedback()))
+        getExternalFeedback()?.let { baseNode.reportFeedbacks(listOf(it)) } ?: baseNode.reportFeedbacks(feedbacks)
     }
 
-    abstract fun getExternalFeedback(): FeedbackType
+    abstract fun getExternalFeedback(): FeedbackType?
 }
