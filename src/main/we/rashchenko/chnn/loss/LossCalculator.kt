4d2c077ba859690adac49a13f05b7a14f984b1bc
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-10-20 13:07:46 +0300

    further stripping: remove loss calculator

--- src/main/we/rashchenko/chnn/loss/LossCalculator.kt
+++
@@ -1,7 +0,0 @@
-package we.rashchenko.chnn.loss
-
-import we.rashchenko.chnn.network.Network
-
-abstract class LossCalculator<ActivationType, FeedbackType>(private val network: Network<ActivationType, FeedbackType>) {
-    abstract fun calculateLoss(): Double
-}
