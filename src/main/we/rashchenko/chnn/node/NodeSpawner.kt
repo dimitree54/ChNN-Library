20f6dbd77702d760e14bbf5d1b36bf080e7e1a68
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-10-20 11:50:21 +0300

    support of initial activity specifying in node spawner

--- src/main/we/rashchenko/chnn/node/NodeSpawner.kt
+++ src/main/we/rashchenko/chnn/node/NodeSpawner.kt
@@ -3,4 +3,5 @@ package we.rashchenko.chnn.node
 interface NodeSpawner<ActivationType, FeedbackType> {
     fun spawn(): Node<ActivationType, FeedbackType>
     fun spawn(node: Node<ActivationType, FeedbackType>): Node<ActivationType, FeedbackType>
+    fun spawn(initialActivity: ActivationType): Node<ActivationType, FeedbackType>
 }
