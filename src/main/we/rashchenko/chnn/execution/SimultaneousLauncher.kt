dd4f8d4a7eef5c17a25b277c65970af45726011b
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-10-22 11:18:43 +0300

    update SimultaneousLauncher

--- src/main/we/rashchenko/chnn/execution/SimultaneousLauncher.kt
+++ src/main/we/rashchenko/chnn/execution/SimultaneousLauncher.kt
@@ -1,10 +1,24 @@
 package we.rashchenko.chnn.execution
 
+import we.rashchenko.chnn.environment.Environment
+import we.rashchenko.chnn.environment.ExternallyControlledNode
 import we.rashchenko.chnn.network.Network
 
 class SimultaneousLauncher<ActivationType, FeedbackType>(
     private val network: Network<ActivationType, FeedbackType>,
+    private val environment: Environment<ActivationType>,
+    private val connectors: List<ExternallyControlledNode<ActivationType, FeedbackType>>
 ) {
+
+    init {
+        updateConnectors()
+    }
+
+    private fun updateConnectors() {
+        connectors.zip(environment.getState()).forEach { (connector, state) ->
+            connector.externalActivity = state
+        }
+    }
 
     private fun gatherAllInputs(): Map<Int, Map<Int, ActivationType>>{
         return network.getAllIds().associateWith { network.gatherInputs(it) }
@@ -20,5 +34,11 @@ class SimultaneousLauncher<ActivationType, FeedbackType>(
         network.getAllIds().forEach { nodeId ->
             network.touch(nodeId, allFeedbacks[nodeId]!!, allInputs[nodeId]!!)
         }
+    }
+
+    fun update(){
+        network.update()
+        environment.tick()
+        updateConnectors()
     }
 }
