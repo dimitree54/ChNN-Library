8d6954b28e1422e377d97ed337b71c64ff6712cb
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-12-03 21:17:59 +0200

    move ExternallyControlledNeuron from aang to library

---
+++ src/main/we/rashchenko/chnn/environment/ExternallyControlledNeuron.kt
@@ -0,0 +1,36 @@
+package we.rashchenko.chnn.environment
+
+import we.rashchenko.chnn.node.Activity
+import we.rashchenko.chnn.node.SmartNeuron
+
+abstract class ExternallyControlledNeuron<EnvironmentActivationType, NeuronActivationType, FeedbackType, ConnectionRequestType>(
+    private val externalActivity: Activity<EnvironmentActivationType>,
+    private val baseNeuron: SmartNeuron<NeuronActivationType, FeedbackType, ConnectionRequestType>,
+) : SmartNeuron<NeuronActivationType, FeedbackType, ConnectionRequestType> by baseNeuron {
+
+    override val activity: NeuronActivationType
+        get() = fuseExternalActivityIntoNeuron(externalActivity.activity, baseNeuron.activity)
+
+    protected abstract fun fuseExternalActivityIntoNeuron(
+        externalActivity: EnvironmentActivationType, neuronActivity: NeuronActivationType,
+    ): NeuronActivationType
+
+    override fun listenForFeedbacks(feedbacks: Collection<FeedbackType>) {
+        baseNeuron.listenForFeedbacks(listOf(getExternalFeedback(externalActivity.activity, baseNeuron.activity)))
+    }
+
+    protected abstract fun getExternalFeedback(
+        externalActivity: EnvironmentActivationType, neuronActivity: NeuronActivationType,
+    ): FeedbackType
+
+    override fun toString(): String {
+        return "$externalActivity ${super.toString()}"
+    }
+
+    val externalActivation: EnvironmentActivationType
+        get() = externalActivity.activity
+
+    val internalActivation: NeuronActivationType
+        get() = baseNeuron.activity
+}
+
